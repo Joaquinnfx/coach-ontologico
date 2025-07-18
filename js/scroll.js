8319cdd558b7d0ccb99bcefbfe6859477f011631
Federico Lopez <lopezfedericojavier1@gmail.com>
2025-07-18 18:48:50 -0300

    funcionalidad-form

--- js/scroll.js
+++ js/scroll.js
@@ -10,3 +10,25 @@ document.querySelectorAll('.nav-link').forEach(link => {
       }
     });
   });
+
+  const form = document.getElementById("form-contacto");
+  const estado = document.getElementById("estadoMensaje");
+
+  form.addEventListener("submit", async function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const res = await fetch(form.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    });
+
+    if (res.ok) {
+      estado.innerText = "¡Mensaje enviado!";
+      form.reset();
+    } else {
+      estado.innerText = "Error al enviar. Probá más tarde.";
+    }
+  });
